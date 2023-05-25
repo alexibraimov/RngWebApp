0f5d33b6fc12bc0339aac0351baca480bf769c2d
alexibraimov <alexibraimov@icloud.com>
2023-05-25 20:23:55 +0600

    add app

---
+++ NumberHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RngApp;
+public class NumberHub : Hub
+{
+    public async Task SendNumber(int number)
+    {
+        await Clients.All.SendAsync("ReceiveNumber", number);
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        await base.OnConnectedAsync();
+        await SendNumber(Random.Shared.Next(0, 1000));
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace RngApp;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSignalR();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/api/getNumber", async context =>
+            {
+                int firstNumber = Random.Shared.Next(0, 1000);
+                await context.Response.WriteAsync(firstNumber.ToString());
+            });
+
+            endpoints.MapHub<NumberHub>("/numberHub");
+
+            endpoints.MapGet("/", async context =>
+            {
+                context.Response.Redirect("/index.html");
+            });
+
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
